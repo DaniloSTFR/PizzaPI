635716ed6c7cbe8af8e5ed209a898ce58e836458
Danilo Santos <danilo.santosfr@gmail.com>
2020-09-15 18:18:23 -0300

    Aquivos Controler, Menus e BootStrap

---
+++ controladores/produtos/produtos.inserir.php
@@ -0,0 +1,6 @@
+<nav aria-label="breadcrumb">
+  <ol class="breadcrumb">
+    <li class="breadcrumb-item"><a href="#">Produtos</a></li>
+    <li class="breadcrumb-item active" aria-current="page">Adicionar</li>
+  </ol>
+</nav>
